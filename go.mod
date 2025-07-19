babff7977f10b76fe880a492ada415004ac46c8a
Dicky Herlambang (花） <herlambangdicky5@gmail.com>
2025-07-19 16:15:04 +0700

    Go: Belajar sampai awal database
    Signed-off-by: Dicky Herlambang (花） <herlambangdicky5@gmail.com>

--- go.mod
+++ go.mod
@@ -1,3 +1,16 @@
 module golang-init
 
 go 1.24.5
+
+require github.com/jackc/pgx v3.6.2+incompatible
+
+require (
+	filippo.io/edwards25519 v1.1.0 // indirect
+	github.com/go-sql-driver/mysql v1.9.3 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
+	github.com/jackc/pgx/v5 v5.7.5 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	golang.org/x/crypto v0.37.0 // indirect
+	golang.org/x/text v0.24.0 // indirect
+)
--- main.go
+++ main.go
@@ -2,17 +2,61 @@ package main
 
 import (
 	"encoding/base64"
+	"encoding/json"
 	"fmt"
+	"html/template"
+	"net/http"
+	"net/url"
+	"os"
 	"os/exec"
 	"regexp"
 	"runtime"
 	"strconv"
 	"time"
+
+	"github.com/jackc/pgx"
 ) 
+
+var baseSvr = "xxxx"
+var secretsauce = "xxxx"
+var databaseUrl = "xxxx"
+
+var data = []student{
+	{"E001", "Alice", 90},
+	{"E002", "Bob", 85},
+	{"E003", "Charlie", 92},
+	{"F001", "David", 88},
+	{"F002", "Eva", 95},
+}
+
+type User struct {
+	Name string `json:"name"`
+	Age  int    `json:"age"`
+	City string `json:"city"`
+}
+
+type student struct {
+	ID string
+	Name string
+	Grade int
+}
+
+type changelog struct {
+	ReleaseDate string   `json:"release_date"`
+	Version     string   `json:"version"`
+	Changelog   []string `json:"changelog"`
+}
+
+type changelogResponse struct {
+	Status  int         `json:"status"`
+	Message string      `json:"message"`
+	App     interface{} `json:"app"`
+	Data    []changelog `json:"data"`
+}
 
 func main() {
 	runtime.GOMAXPROCS(2) // Set jumlah goroutine yang berjalan bersamaan
-	execution()
+	dbTest()
 }
 
 func array() {
@@ -122,4 +166,191 @@ func execution() {
 			fmt.Println("Error executing command: ", err.Error())
 		}
 	}
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, I'm learning Golang!")
+}
+
+func handleIndex() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "Heloo from Golang!")
+	})
+
+	http.HandleFunc("/home", func(w http.ResponseWriter, r *http.Request) {
+		var data = map[string]string{
+			"Name":	"John Doe",
+			"Message": "Welcome to the home page!",
+		}
+
+		var homeTemp,err = template.ParseFiles("template.html")
+		if err != nil {
+			fmt.Println("Error parsing template:", err)
+			return
+		}
+		homeTemp.Execute(w, data)
+	})
+
+	http.HandleFunc("/api/user", WebAPILearnUser)
+	http.HandleFunc("/api/users", WebAPILearnUsers)
+
+	http.HandleFunc("/index", index)
+
+	fmt.Println("Server is running on port 8080")
+	http.ListenAndServe(":8080", nil)
+}
+
+func urlParsing() {
+	var urlString = "https://pdf.hana-ci.com/compress"
+	var uri, err = url.Parse(urlString)
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	fmt.Printf("Current Url is %s\n", uri.String())
+	fmt.Printf("Protocol is %s\n", uri.Scheme)
+	fmt.Printf("Host is %s\n", uri.Host)
+	fmt.Printf("Path is %s\n", uri.Path)
+}
+
+func jsonLearn() {
+	var jsonString = `{"name": "John", "age": 30, "city": "New York"}`
+	var jsonString2 = `[
+		{"name": "Jane", "age": 25, "city": "Los Angeles"},
+		{"name": "Mike", "age": 32, "city": "Chicago"},
+		{"name": "Sara", "age": 28, "city": "Miami"}
+	]`
+
+	_ = []byte(jsonString)
+	var jsonData2 = []byte(jsonString2)
+
+	var data []User
+
+	var err = json.Unmarshal(jsonData2, &data)
+	if err != nil {
+		fmt.Println("Unmarshal error:", err)
+		return
+	}
+
+	fmt.Println("Name:", data[2].Name)
+	fmt.Println("Age:", data[2].Age)
+	fmt.Println("City:", data[2].City)
+}
+
+func WebAPILearnUsers(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	if r.Method == http.MethodGet {
+		var result, err = json.Marshal(data)
+			if err != nil {
+				http.Error(w, "Error marshaling data", http.StatusInternalServerError)
+				return
+			}
+
+			w.Write(result)
+			return
+	}
+
+	http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+}
+
+func WebAPILearnUser(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	if r.Method == http.MethodGet {
+		var id = r.FormValue("id")
+			var result []byte
+			var err error
+
+			for _, each := range data {
+				if each.ID == id {
+					result, err = json.Marshal(each)
+					if err != nil {
+						http.Error(w, "Error marshaling data", http.StatusInternalServerError)
+						return
+					}
+					w.Write(result)
+					return
+				}
+			}
+
+			http.Error(w, "User not found", http.StatusNotFound)
+			return
+	}
+
+	http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+}
+
+func HttpRequestLearn()(changelogResponse, error) {
+	var err error
+	var client = &http.Client{}
+	var data changelogResponse
+
+	request, err := http.NewRequest("GET", baseSvr+"/api/v1/version/fetch", nil)
+	if err != nil {
+		fmt.Println("Error creating request:", err)
+		return data, err
+	}
+	request.Header.Set("Content-Type", "application/json")
+	request.Header.Set("Accept", "application/json")
+	request.Header.Set("Authorization", "Bearer "+secretsauce)
+	request.Header.Set("User-Agent", "Golang-HTTP-Client/1.0")
+	request.Header.Set("X-Requested-With", "XMLHttpRequest")
+
+	response, err := client.Do(request)
+	if err != nil {
+		fmt.Println("Error making request:", err)
+		return data, err
+	}
+	defer response.Body.Close()
+
+	err = json.NewDecoder(response.Body).Decode(&data)
+	if err != nil {
+		fmt.Println("Error decoding response:", err)
+		return data, err
+	}
+
+	return data, nil
+}
+
+func callHttpRequestLearn() {
+	data, err := HttpRequestLearn()
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+
+	for _, change := range data.Data {
+		fmt.Printf("Release Date: %s\n", change.ReleaseDate)
+		fmt.Printf("Version: %s\n", change.Version)
+		fmt.Printf("Changelog:",)
+		for _, changelog := range change.Changelog {
+			fmt.Printf(" - %s\n", changelog)
+		}
+	}
+}
+
+func dbTest() {
+	connConfig, err := pgx.ParseURI(databaseUrl)
+	if err != nil {
+		fmt.Println("Error parsing database URL:", err)
+		return
+	}
+
+	conn, err := pgx.Connect(connConfig)
+	if err != nil {
+		fmt.Println("Error connecting to database:", err)
+		return
+	}
+	defer conn.Close()
+
+	var username, password string
+	err = conn.QueryRow("select name, password from users").Scan(&username, &password)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Query Row Failed: %v\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Println(username, password)
 }
